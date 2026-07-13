0eb100eff3fd274d963798d7063efa3e8f4fe0aa
Kamel ZOUBA <artilog.it@gmail.com>
2026-07-13 16:30:56 +0200

    [PROJET2] appel ws huggingface

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
-token = os.getenv("TOKEN_HUGGINGFACE")
-print(token)
+URL_HUGGINGFACE = os.getenv("URL_HUGGINGFACE")
+
+TOKEN_HUGGINGFACE = os.getenv("TOKEN_HUGGINGFACE")
+
+
+
+headers = {"Authorization": f"Bearer {TOKEN_HUGGINGFACE}",
+           "Content-Type": "image/png"}
+
+def appelHuggingFace(image_path):
+    with open(image_path, "rb") as f:
+        data = f.read()
+    response = requests.post(URL_HUGGINGFACE, headers=headers, data=data)
+    return response.json()
+
+result = appelHuggingFace("./top_influenceurs_2024/IMG/image_0.png")
+print(result)
